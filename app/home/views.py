ad370101c0214b4491bf95f430153570b71e8f55
alexhong33 <alexhongf@163.com>
2017-07-31 16:25:32 +0800

    templates update

--- app/home/views.py
+++ app/home/views.py
@@ -6,9 +6,65 @@
 # @Description : STOP wishing START doing. 
 
 from . import home
-from flask import render_template
+from flask import render_template, redirect, url_for
 
+
+# 创建视图
+
+
+
+
+@home.route("/login/")
+def login():
+    return render_template("home/login.html")
+
+
+@home.route("/logout/")
+def logout():
+    return redirect(url_for("home.login"))  # 返回到登录页面
+
+
+@home.route("/regist/")
+def regist():
+    return render_template("home/regist.html")
+
+
+@home.route("/user/")
+def user():
+    return render_template("home/user.html")
+
+
+@home.route("/pwd/")
+def pwd():
+    return render_template("home/pwd.html")
+
+
+@home.route("/comments/")
+def comments():
+    return render_template("home/comments.html")
+
+
+@home.route("/loginlog/")
+def loginlog():
+    return render_template("home/loginlog.html")
+
+
+@home.route("/moviecol/")
+def moviecol():
+    return render_template("home/moviecol.html")
+
+
+# 指向首页
 @home.route("/")
 def index():
     return render_template("home/index.html")
- 
+
+
+# 指向animation
+@home.route("/animation/")
+def animation():
+    return render_template("home/animation.html")
+
+@home.route("/search/")
+def search():
+    return render_template("home/search.html")
