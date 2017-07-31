ad01c0157667f5e893481721104d1e174973b9a0
alexhong33 <alexhongf@163.com>
2017-07-31 17:09:26 +0800

    前台页面搭建完毕

--- app/home/views.py
+++ app/home/views.py
@@ -65,6 +65,12 @@ def index():
 def animation():
     return render_template("home/animation.html")
 
+
 @home.route("/search/")
 def search():
     return render_template("home/search.html")
+
+
+@home.route("/play/")
+def play():
+    return render_template("home/play.html")
